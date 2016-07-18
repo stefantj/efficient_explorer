dc4e7c34a004b70ba40336d110ba87c65ec7a0fb
Stefan Jorgensen <stefantj@stanford.edu>
2016-07-18 09:31:21 -0700

    Initial Commit

---
+++ FMT/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  FMT
+//
+//  Created by Megamind on 7/18/16.
+//  Copyright (c) 2016 ASL. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
